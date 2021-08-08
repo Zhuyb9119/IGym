e6ba64e805710713e25a217ceafe1938716c62bc
Mniwyl <wenyule@163.com>
2021-08-08 13:48:49 +0800

    仓储添加

---
+++ src/IGym.IRepository/Model/IUser.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IGym.IRepository.Model
+{
+    public interface IUser
+    {
+        int Id { get; set; }
+    }
+}
