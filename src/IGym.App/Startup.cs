6004d5dbf4ec61ad2f965a85de767de0a6deba71
朱元彬 <bins_123@live.com>
2021-08-09 23:00:20 +0800

    jwt生成Demo

--- src/IGym.App/Startup.cs
+++ src/IGym.App/Startup.cs
@@ -1,3 +1,6 @@
+using IGym.EFRepository.Impl;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -6,9 +9,11 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace IGym
@@ -26,6 +31,31 @@ namespace IGym
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            #region JWT
+            
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["SecurityKey"]));
+            services.AddSingleton(securityKey);
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options =>
+                {
+                    //Cookie方案
+                })
+                .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
+                 {
+                     options.TokenValidationParameters = new TokenValidationParameters
+                     {
+                         ValidateIssuer = true,                     //是否验证Issuer
+                         ValidateAudience = true,                   //是否验证Audience
+                         ValidateLifetime = true,                   //是否验证失效时间
+                         ClockSkew = TimeSpan.FromSeconds(30),      //偏离时间，在此时间内还可以使用
+                         ValidateIssuerSigningKey = true,           //是否验证SecurityKey
+                         ValidAudience = "localhost",               //Audience
+                         ValidIssuer = "localhost",                 //Issuer
+                         IssuerSigningKey = securityKey             //拿到SecurityKey
+                     };
+                 });
+
+            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,6 +69,8 @@ namespace IGym
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
