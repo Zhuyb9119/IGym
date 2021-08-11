b3ad5b12cb8ddfc5c5b710f1fd8a4832fe61d628
朱元彬 <bins_123@live.com>
2021-08-11 19:03:56 +0800

    添加授权，Jwt相关，Secret用户相关

---
+++ src/IGym.Application/Authorization/Jwt/IJwtAppService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IGym.Application.Authorization.Jwt
+{
+    interface IJwtAppService
+    {
+
+    }
+}
---
+++ src/IGym.Application/Authorization/Secret/SecretAppService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IGym.Application.Authorization.Secret
+{
+    class SecretAppService:ISecretAppService
+    {
+    }
+}
